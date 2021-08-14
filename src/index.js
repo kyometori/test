00e0239cfe09a4db1622e30e21eaf3f61116b034
kyometori <ac0xrpfs001@vivladi.net>
2021-08-14 15:17:43 +0800

    add some animations

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
 import './style.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
+  <HashRouter><App /></HashRouter>,
   document.getElementById('root')
 );
+
+document.getElementById('switch').addEventListener('click', () => {
+  const nav = document.getElementById('nav');
+
+  if (nav.clientWidth > 0) {
+    nav.className = "collapsed";
+  } else {
+    nav.className = "open";
+  }
+
+});
