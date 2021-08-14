15c4413a366e0fe28d89252f6fb7b9830aee1b19
kyometori <ac0xrpfs001@vivladi.net>
2021-08-14 15:17:43 +0800

    add some animations

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Link, Switch, Route } from 'react-router-dom';
+import { Link, Switch, Route, Redirect } from 'react-router-dom';
 import mpIcon from './img/nav-icons/mainpage.png';
 import annIcon from './img/nav-icons/announcement.png';
 
@@ -19,7 +19,7 @@ function NavLink(props) {
 
 export function Nav() {
   return (
-    <div id="nav"  className='pc-only'>
+    <div id="nav" className='pc-only'>
       <NavLink icon={mpIcon} url='' text='首頁' />
       <NavLink icon={annIcon} url='/announcement' text='最新公告' />
     </div>
@@ -32,10 +32,10 @@ export function Content() {
       <Route exact path='/test'>
         welcome
       </Route>
-      <Route path='/test/invite'>
-        invite
+      <Route exact path='/test/invite'>
+        <Redirect to={{pathname: "https://hizollo.ddns.net/invite"}} />
       </Route>
-      <Route path='/test/server'>
+      <Route exact path='/test/server'>
         server
       </Route>
       <Route path='/test'>
