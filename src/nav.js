1726f4c4439a72efbdc7d9df094e190383b9feba
kyometori <ac0xrpfs001@vivladi.net>
2021-08-27 22:06:22 +0800

    add more nav

--- src/nav.js
+++ src/nav.js
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
 import mpIcon from './img/nav-icons/mainpage.png';
 import annIcon from './img/nav-icons/announcement.png';
+import cmdIcon from './img/nav-icons/command.png';
+import tutorialIcon from './img/nav-icons/tutorial.png';
+import changelogIcon from './img/nav-icons/changelog.png';
 
 function NavLink(props) {
   return (
@@ -22,6 +25,9 @@ export default function Nav() {
     <div id="nav" className='pc-only'>
       <NavLink icon={mpIcon} url='/' text='首頁' />
       <NavLink icon={annIcon} url='/announcement' text='最新公告'/>
+      <NavLink icon={cmdIcon} url='/commands' text='指令列表'/>
+      <NavLink icon={tutorialIcon} url='/tutorials' text='使用教學'/>
+      <NavLink icon={changelogIcon} url='/changelog' text='更新日誌'/>
     </div>
   )
 }
