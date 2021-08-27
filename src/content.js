c766b684b3beb32078485d633347d88ff29bd45a
kyometori <ac0xrpfs001@vivladi.net>
2021-08-27 21:49:38 +0800

    finish the scroll content

--- src/content.js
+++ src/content.js
@@ -1,28 +1,31 @@
 import { Switch, Route } from 'react-router-dom';
 import Redirect from './redirects.js';
+import Mainpage from './pages/mainpage/index.js';
 
 export default function Content(props) {
   return (
-    <Switch>
-      <Route exact path='/'>
-        welcome
-      </Route>
-      <Route exact path='/dst'>
-        <Redirect url='https://discordservers.tw/bots/584677291318312963' />
-      </Route>
-      <Route exact path='/invite'>
-        <Redirect url='https://hizollo.ddns.net/invite' />
-      </Route>
-      <Route exact path='/server'>
-        <Redirect url='https://discord.com/invite/xUXTrYG2MZ' />
-      </Route>
-      <Route exact path='/source'>
-        <Redirect url='https://github.com/kyometori/test' />
-      </Route>
-      <Route exact path='/:thing' render={props => props.match.params.thing} />
-      <Route path='/'>
-      (´・ω・`)
-      </Route>
-    </Switch>
+    <div id="content">
+      <Switch>
+        <Route exact path='/'>
+          <Mainpage />
+        </Route>
+        <Route exact path='/dst'>
+          <Redirect url='https://discordservers.tw/bots/584677291318312963' />
+        </Route>
+        <Route exact path='/invite'>
+          <Redirect url='https://hizollo.ddns.net/invite' />
+        </Route>
+        <Route exact path='/server'>
+          <Redirect url='https://discord.com/invite/xUXTrYG2MZ' />
+        </Route>
+        <Route exact path='/source'>
+          <Redirect url='https://github.com/kyometori/test' />
+        </Route>
+        <Route exact path='/:thing' render={props => props.match.params.thing} />
+        <Route path='/'>
+        (´・ω・`)
+        </Route>
+      </Switch>
+    </div>
   )
 }
